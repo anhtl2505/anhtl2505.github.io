55191215d73ae923ded38fd75bea0cd620781b54
anhtl2505 <anhtl2505@gmail.com>
2018-07-26 15:49:49 +0700

    part 4

---
+++ Ex/effect/js/1.js
@@ -0,0 +1,3 @@
+$(function() {
+	TweenMax.staggerFrom($('.khunganh1, .khunganh2, .khunganh3'),1,{top:100,opacity:0},0.4)
+});
---
+++ Ex/game/1.js
@@ -0,0 +1,22 @@
+﻿ 
+
+/*-------------------------------------------------------------------------
+*/ $(function(){
+ 	$('.navbar-right li:nth-child(2) a')
+ 	.click(function(event) {
+ 		/* Act on the event */
+ 		$('body').animate({scrollTop:750})
+ 		return false;
+ 	});
+
+	 $('.navbar-right li:nth-child(3) a')
+ 	.click(function(event) {
+ 		/* Act on the event */
+ 		$('body').animate({scrollTop:1700})
+ 		return false;
+ 	});
+	 
+})  
+ 
+
+
